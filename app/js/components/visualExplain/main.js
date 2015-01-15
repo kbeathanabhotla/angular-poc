35375464a12f18bdb8e29302147cd9967fa2af13
saikrishna-kishore <krishna.sai6491@gmail.com>
2015-01-16 04:52:09 +0530

    moved get visual explain content into a method

--- app/js/components/visualExplain/main.js
+++ app/js/components/visualExplain/main.js
@@ -5,25 +5,28 @@ app.directive('visualExplain', ['VisualExplainService', 'QueryService', function
     restrict    : 'E',
     controller : [ '$scope', '$location' , function($scope, $location) {
 
-    	//$location.search()
-    	var queryId = $location.search().queryId;
-    	console.log('query Id : '+queryId);
-
-    	var visualStructure;
-
-    	if(queryService.getQuery(queryId).visualStructure) {
-            visualStructure = queryService.getQuery(queryId).visualStructure;
-        } else {
-            queryService.getQuery(queryId).visualStructure = '';
-            visualExplainService.visualExplain(queryService.getQuery(queryId).queryText)
-            .then(function(result) {
-                console.log('got result : '+JSON.stringify(result.data));
-                visualStructure = result.data;
-                queryService.getQuery(queryId).visualStructure = visualStructure;
-            });
+        function getVisualStructure(queryId) {
+            if(queryService.getQuery(queryId).visualStructure) {
+                return queryService.getQuery(queryId).visualStructure;
+            } else {
+                queryService.getQuery(queryId).visualStructure = '';
+                visualExplainService.visualExplain(queryService.getQuery(queryId).queryText)
+                .then(function(result) {
+                    console.log('got result : '+JSON.stringify(result.data));
+                    visualStructure = result.data;
+                    queryService.getQuery(queryId).visualStructure = visualStructure;
+                    return visualStructure;
+                });
+            }
         }
 
-        console.log('visual structure is : '+visualStructure);
+        var visualStructure = getVisualStructure($location.search().queryId);
+
+        if(visualStructure) {
+            console.log('visual structure is : '+visualStructure);    
+        }
+        
+    	
 
     }] 
   };
