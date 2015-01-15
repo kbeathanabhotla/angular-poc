589b438f13b6857dfc19789cee3d5f71793226bb
saikrishna-kishore <krishna.sai6491@gmail.com>
2015-01-15 14:49:17 +0530

    added loading animation

---
+++ app/js/components/loadingAnimation/main.js
@@ -0,0 +1,24 @@
+app.directive('loadingAnimation',   ['$http' ,function ($http)
+    {
+        return {
+            templateUrl : 'js/components/loadingAnimation/template.html',
+            restrict: 'A',
+            link: function (scope, elm, attrs)
+            {
+                scope.isLoading = function () {
+                    return true;
+                    //return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v)
+                {
+                    if(v){
+                        elm.show();
+                    }else{
+                        elm.hide();
+                    }
+                });
+            }
+        };
+
+    }]);
--- app/js/services/queryService.js
+++ app/js/services/queryService.js
@@ -1,11 +1,11 @@
 app.service('QueryService', [ '$http' ,function($http) {
 	
 	this.getExplainXML = function(query) {
-	   $http.get('http://echo.jsontest.com/conditions/frightful').then(function(resp) {
+	   $http.get('http://localhost:8080/test').then(function(resp) {
         console.log('got data from Backend as : '+JSON.stringify(resp.data));
   	 }, function(err) {
       	console.error('ERR', err);
     	 // err.status will contain the status code
-  	 })
+  	 });
 	};
 }]);
